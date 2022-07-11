5c826407cf0811aba22b9ea53cd323bed54ca7e2
edward cardew <u6379162@anu.edu.au>
2022-07-11 12:12:42 +1000

    july11

--- Move_Enge.py
+++ Move_Enge.py
@@ -1,14 +1,28 @@
 import time
 import sys
 import RPi.GPIO as GPIO
+import signal
+
+
+pulse_pin = 37
+direction_pin = 40
+killswitch = ##
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(37, GPIO.OUT, initial= GPIO.HIGH)
-GPIO.setup(40, GPIO.OUT)
+GPIO.setup(pulse_pin, GPIO.OUT, initial= GPIO.HIGH)
+GPIO.setup(direction_pin, GPIO.OUT)
+GPIO.add_event_detect(killswitch, GPIO.FALLING, callback= overshoot)
 
 def get_current():
     file = open("current_pos.txt", "r")
     past = int(file.read())
     return past
+
+def overshoot():
+    if GPIO.input(killswitch):
+        print('STOP: Detector has exceeded limit of operations')
+        GPIO.output(pulse_pin, GPIO.LOW)
+
     
 
 def save_value(value, direction):
@@ -26,23 +40,23 @@ def save_value(value, direction):
     
 
 def move_forward_1(steps):
-    GPIO.output(40, GPIO.LOW)
+    GPIO.output(direction_pin, GPIO.LOW)
     x = 1
     while x<steps*320+1:
-        GPIO.output(37, GPIO.LOW)
+        GPIO.output(pulse_pin, GPIO.LOW)
         time.sleep(.001)
-        GPIO.output(37, GPIO.HIGH)
+        GPIO.output(pulse_pin, GPIO.HIGH)
         time.sleep(.001)
         x = x+1
-    GPIO.output(40, GPIO.HIGH)
+    GPIO.output(direction_pin, GPIO.HIGH)
         
 def move_back_1(steps):
-    GPIO.output(40,GPIO.HIGH)
+    GPIO.output(direction_pin,GPIO.HIGH)
     x = 1
     while x<320*steps+1:
-        GPIO.output(37, GPIO.HIGH)
+        GPIO.output(pulse_pin, GPIO.HIGH)
         time.sleep(0.001)
-        GPIO.output(37, GPIO.LOW)
+        GPIO.output(pulse_pin, GPIO.LOW)
         time.sleep(0.001)
         x = x+1
         
